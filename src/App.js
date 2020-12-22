d155d113c7b46d6b3de61c71ebc5c0a67e98d2c7
AleksLat <66917008+AleksLat@users.noreply.github.com>
2020-12-22 12:59:19 +0100

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ export default function App() {
   return (
     <div className="App">
       Belarus free
+      <footer>
+        This project was coded by {" "}
+        <a href="https://www.linkedin.com/in/aleks-lat/"
+        target="_blank">
+          Aleks Lat{" "}
+        </a>
+         and is{" "}
+      <a href="https://github.com/AleksLat/react-weather-blr-app"
+      target="_blank">  
+      open-sourced on GitHub
+      </a>
+      </footer>
+     
     </div>
   );
 }
